1f2998508bf3e13b0e5412615e850d654c758a5f
jgunn <j.s.gunn22@gmail.com>
2023-09-19 13:48:46 -0700

    sets up core navigation

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,14 @@
 const router = require("express").Router();
 
+router.get("/", async (req, res) => {
+  //
+  // will fetch all active games
+  //
+
+  // temp to render page
+  res.render("lobby", {
+    loggedIn: req.session.loggedIn, // if user is logged in passed into handlebar
+  });
+});
 // was throwing error without the Router() middleware being passed into server.js
 module.exports = router;
