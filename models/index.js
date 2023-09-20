ad9ea0245b95804620381a63269d986d5bc2b735
jgunn <j.s.gunn22@gmail.com>
2023-09-20 11:28:28 -0700

    removes commented code

--- models/index.js
+++ models/index.js
@@ -1,12 +1,6 @@
 const User = require("./User");
 const Game = require("./Game");
 
-// //a single user can have many roles in many games
-// User.belongsToMany(Game, { through: UserGameRole, foreignKey: "user_id" });
-
-// //a game can have many users with various roles
-// Game.belongsToMany(User, { through: UserGameRole, foreignKey: "game_id" });
-
 //users can be attackers for many games
 User.hasMany(Game, { foreignKey: "attacker_id" });
 Game.belongsTo(User, { as: "Attacker", foreignKey: "attacker_id" });
