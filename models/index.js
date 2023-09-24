8e6fe169d0cb333817c9d4f1c7cbaa1f0d9d63b3
Bryan Bickel <briimcfly@gmail.com>
2023-09-24 12:42:52 -0700

    added Chat model, added chat relationship to index

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Game = require("./Game");
+const Chat = require("./Chat");
 
 //users can be defenders for many games
 User.hasMany(Game, { as: "Defender", foreignKey: "defender_id" });
@@ -9,7 +10,12 @@ Game.belongsTo(User, { as: "Defender", foreignKey: "defender_id" });
 User.hasMany(Game, { as: "Attacker", foreignKey: "attacker_id" });
 Game.belongsTo(User, { as: "Attacker", foreignKey: "attacker_id" });
 
+//Chat messages belong to games 
+Chat.belongsTo(Game, {foreignKey: 'game_id'});
+Game.hasMany(Chat, {foreignKey: 'game_id'});
+
 module.exports = {
   User,
   Game,
+  Chat
 };
