fb0708646986eba3cfab8ceeb60f35cb641c98b3
Kyle Jocoy <kjocoy@gmail.com>
2023-09-25 15:38:45 -0700

    saving to pull from main

--- public/js/leaveGame.js
+++ public/js/leaveGame.js
@@ -5,23 +5,25 @@ const forfeitGameBtn = document.querySelector("#forfeit-game-btn");
 async function forfeitGame(event) {
   event.preventDefault();
 
+  
+  // This double stacks the API route when in a game. Only works in main lobby, which will then not give the game ID. 
+  const getMyId = await fetch("/api/users/my-id", {
+    method: "GET",
+  });
+  const myId = await getMyId.json();
+  console.log("This is the player ID: " + myId);
+  
   // Game ID from the window, can be seen in the URL path
   const pathArray = window.location.pathname.split('/');
   const gameId = pathArray[pathArray.length-1];
   console.log("This is the game ID: " + gameId);
 
-  // This double stacks the API route when in a game. Only works in main lobby, which will then not give the game ID. 
-  const getMyId = await fetch("api/users/my-id", {
-    method: "GET",
-  });
-  const myId = await getMyId.json();
-  console.log("This is the player ID: " + myId);
-
-
-  const getMyGame = await fetch("api/games/my-games", {
+  const getMyGame = await fetch(`/api/games/${gameId}`, {
     method: "GET",
   });
   const myGame = await getMyGame.json();
+  console.log("Current attacker ID: " + myGame.attacker_id);
+  console.log("Current defender ID: " + myGame.defender_id);
   // get the game ID from the DOM
   /*  const pathArray = window.location.pathname.split('/');
   const gameId = pathArray[pathArray.length-1];
@@ -38,7 +40,6 @@ async function forfeitGame(event) {
   // If the player is forgeiting as the defender, increase their losses. Up attacker's wins
 
   console.log("Forfeit button was pressed");
-  console.log("Current game data:" + myGame);
 }
 
 forfeitGameBtn.addEventListener("click", forfeitGame)
